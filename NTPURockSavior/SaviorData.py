7812f098c707545d1f667b9fca28434b302c3df4
amadeus0417 <oscar83155@gmail.com>
2024-01-20 03:51:24 +0800

    added class 'SaviorData'

---
+++ NTPURockSavior/SaviorData.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class SaviorData:
+    
+    def __init__(self):
+        self.account = tk.StringVar()
+        self.password = tk.StringVar()
+        self.website = tk.StringVar()
+        self.gsheet = tk.StringVar()
+        self.wsheet = tk.StringVar()
+        self.hsheet = tk.StringVar()
+        self.hwsheet = tk.StringVar()
+        self.gsjson = tk.StringVar()
--- NTPURockSavior/SaviorUI.py
+++ NTPURockSavior/SaviorUI.py
@@ -3,21 +3,14 @@
 import os
 import os.path
 import tkinter as tk
+import SaviorData
 
 class SaviorUI:
     
-    def __init__(self):
+    def __init__(self, data):
         self.window = tk.Tk()
         self.window.title('NTPU Rock Helper')
         self.window.geometry("800x600")
-        self.account = tk.StringVar()
-        self.password = tk.StringVar()
-        self.website = tk.StringVar()
-        self.gsheet = tk.StringVar()
-        self.wsheet = tk.StringVar()
-        self.hsheet = tk.StringVar()
-        self.hwsheet = tk.StringVar()
-        self.gsjson = tk.StringVar()
         tk.Label(self.window, text= 'Facebook 帳號').grid(row=0, column=0)
         tk.Label(self.window, text= 'Facebook 密碼').grid(row=1, column=0)
         tk.Label(self.window, text= '租借表貼文網址').grid(row=2, column=0)
@@ -26,47 +19,47 @@ class SaviorUI:
         tk.Label(self.window, text='練團時數統計表連結').grid(row=5, column=0)
         tk.Label(self.window, text='統計工作表名稱').grid(row=6,  column=0)
         tk.Label(self.window, text='json 憑證檔案位置').grid(row=7,  column=0)
-        tk.Entry(self.window, textvariable=self.account).grid(row=0, column=1)
-        tk.Entry(self.window, textvariable=self.password).grid(row=1, column=1)
-        tk.Entry(self.window, textvariable=self.website).grid(row=2, column=1)
-        tk.Entry(self.window, textvariable=self.gsheet).grid(row=3, column=1)
-        tk.Entry(self.window, textvariable=self.wsheet).grid(row=4, column=1)
-        tk.Entry(self.window, textvariable=self.hsheet).grid(row=5, column=1)
-        tk.Entry(self.window, textvariable=self.hwsheet).grid(row=6, column=1)
-        tk.Entry(self.window, textvariable=self.gsjson).grid(row=7, column=1)
+        tk.Entry(self.window, textvariable=data.account).grid(row=0, column=1)
+        tk.Entry(self.window, textvariable=data.password).grid(row=1, column=1)
+        tk.Entry(self.window, textvariable=data.website).grid(row=2, column=1)
+        tk.Entry(self.window, textvariable=data.gsheet).grid(row=3, column=1)
+        tk.Entry(self.window, textvariable=data.wsheet).grid(row=4, column=1)
+        tk.Entry(self.window, textvariable=data.hsheet).grid(row=5, column=1)
+        tk.Entry(self.window, textvariable=data.hwsheet).grid(row=6, column=1)
+        tk.Entry(self.window, textvariable=data.gsjson).grid(row=7, column=1)
         tk.Button(self.window, text='確認', command=self.window.destroy).grid(row=8, column=0, columnspan=2)
         try:
-            self.AutoFill()
+            self.AutoFill(data)
         except:
             pass
         self.window.mainloop()
         self.Memorize()
 
 
-    def AutoFill(self):
+    def AutoFill(self, data):
         self.path = os.getenv('temp')
         self.filename = os.path.join(self.path, 'info.txt')
         try:
             with open(self.filename) as fp:
                 acc, pas, web, gsh, wsh, hsh, hws, gsj = fp.read().strip().split(',')
-                self.account.set(acc)
-                self.password.set(pas)
-                self.website.set(web)
-                self.gsheet.set(gsh)
-                self.wsheet.set(wsh)
-                self.hsheet.set(hsh)
-                self.hwsheet.set(hws)
-                self.gsjson.set(gsj)
+                data.account.set(acc)
+                data.password.set(pas)
+                data.website.set(web)
+                data.gsheet.set(gsh)
+                data.wsheet.set(wsh)
+                data.hsheet.set(hsh)
+                data.hwsheet.set(hws)
+                data.gsjson.set(gsj)
         except:
             pass
 
-    def Memorize(self):
+    def Memorize(self, data):
         with open(self.filename, 'w') as fp:
-            fp.write(','.join((self.account.get(),
-                               self.password.get(), 
-                               self.website.get(), 
-                               self.gsheet.get(), 
-                               self.wsheet.get(), 
-                               self.hsheet.get(), 
-                               self.hwsheet.get(), 
-                               self.gsjson.get())))
+            fp.write(','.join((data.account.get(),
+                               data.password.get(), 
+                               data.website.get(), 
+                               data.gsheet.get(), 
+                               data.wsheet.get(), 
+                               data.hsheet.get(), 
+                               data.hwsheet.get(), 
+                               data.gsjson.get())))
