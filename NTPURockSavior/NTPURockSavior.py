2f2e614d3757ab73fde31b8f194041c348e8e67b
amadeus0417 <oscar83155@gmail.com>
2024-01-20 04:01:10 +0800

    流程優化

--- NTPURockSavior/NTPURockSavior.py
+++ NTPURockSavior/NTPURockSavior.py
@@ -1,23 +1,24 @@
 from Savior import NTPURockSavior
+from SaviorData import SaviorData
 from SaviorUI import SaviorUI
 import Sheet
 import time
 
 #import pandas as pd
 #import re, time, requests
+data = SaviorData()
 
-window = SaviorUI()
+window = SaviorUI(data)
 
 savior = NTPURockSavior()
-savior.FaceBookLogin("oscar83155@gmail.com", "amadeus0417")
+savior.FaceBookLogin(data.account, data.password)
+#savior.GetComments(data.website)
 savior.GetComments("https://www.facebook.com/permalink.php?story_fbid=pfbid02ow2NDRJpiJ4hwZx5orxKiT8v5EA8S3HvZXGEFcB5PjkVvdp4vDnBaVULRLPCvFmql&id=100000304782470")
-time.sleep(5)
-#savior.GetComments("https://www.facebook.com/groups/183236594717256/posts/282242764816638/")
 savior.LoadRentInfo()
 
-sheet = Sheet("C:\\Users\oscar\\source\\repos\\Amadeus0417\\NTPURockSavior\\NTPURockSavior\\ntpu-rock-helper.json")
-sheet.OpenSheet("https://docs.google.com/spreadsheets/d/1xXQ2xZev1V2alaQ2T_FBnJrjCJgbugxs2GhMoCCzu6s/edit#gid=0")
-sheet.ChooseWorkSheet("工作表1")
+sheet = Sheet(data.gsjson)
+sheet.OpenSheet(data.gsheet)
+sheet.ChooseWorkSheet(data.wsheet)
 sheet.FillData(savior.rentlist)
 
 
